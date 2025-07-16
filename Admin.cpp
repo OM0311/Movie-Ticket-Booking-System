a455ce0e503d672da411332e31005e34b96ed84e
OM0311 <152823701+OM0311@users.noreply.github.com>
2025-07-15 22:57:23 -0500

    Create Admin.cpp

---
+++ Admin.cpp
@@ -0,0 +1,6 @@
+#include "Admin.h"
+#include <iostream>
+//Displays admin options
+void Admin::displayMenu() {
+    std::cout << "\nAdmin Menu:\n1. Add Movie\n2. Remove Movie\n3. Reset Seats\n4. Exit\n";
+}
